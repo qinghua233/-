a263994e698d7cc3d46eaab8dcc7f6bd0fcd7f35
抱月 <78679900+qinghua233@users.noreply.github.com>
2021-12-12 10:14:48 +0800

    Update BlackCat.java

--- BlackCat.java
+++ BlackCat.java
@@ -2,5 +2,5 @@ package exam;
 
 public class BlackCat extends Cat{
     int price =350;
-        void information(String name,int age,boolean sexy,double price){};
+        void information(String name,int age,boolean sexy,double price,String pingzhong){};
 }
